3d803ad240e9cd24898519532ead727c380d60ea
bayastanchik7 <bayastantursumamatov@gmail.com>
2024-04-01 12:34:57 +0600

    hello

---
+++ src/Pages/MainPages.tsx
@@ -0,0 +1,13 @@
+import HeaderMainLogoPage from '../components/MainPageComponents/HeaderMainLogoPage/HeaderMainLogoPage'
+import HeaderMainPage from '../components/MainPageComponents/HedaerMainPage/HeaderMainPage'
+
+const MainPages: React.FC = () => {
+	return (
+		<div>
+			<HeaderMainPage />
+			<HeaderMainLogoPage />
+		</div>
+	)
+}
+
+export default MainPages
